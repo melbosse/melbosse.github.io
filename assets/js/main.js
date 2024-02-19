eae26d94be52b599d26533b3d75fcbf5297519e3
melbosse <127343358+melbosse@users.noreply.github.com>
2024-02-19 12:10:41 -0500

    Page temporaire - modification du portfolio en cours.
    Mise à jour du portfolio à venir.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,59 @@
-const about = document.querySelector('#about')
-const contact = document.querySelector('#contact')
-const aboutContent = document.querySelector('#about-content')
-const contactContent = document.querySelector('#contact-content')
+/**
+* Template Name: Maundy
+* Updated: Jan 29 2024 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/maundy-free-coming-soon-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
 
-about.addEventListener('click', () => {
-  const aboutBox = new WinBox({
-    title: 'À propos',
-    //modal: true,
-    width: '400px',
-    height: '400px',
-    top: 150,
-    right: 50,
-    bottom: 50,
-    left: 50,
-    mount: aboutContent,
-    onfocus: function () {
-      this.setBackground('#00bbcc')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
 
-contact.addEventListener('click', () => {
-  const contactBox = new WinBox({
-    title: 'Contactez-moi',
-    background: '#00aa00',
-    width: '400px',
-    height: '400px',
-    top: 150,
-    right: 50,
-    bottom: 50,
-    left: 250,
-    mount: contactContent,
-    onfocus: function () {
-      this.setBackground('#00bbcc')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
-
-// Typewriter.js
-// https://github.com/ronv/Typewriter.js & https://github.com/loakeshbachhu/loakeshbachhu.github.io
-
-$.fn.typewriter = function() {
-  this.each(function() {
-    var c = $(this),
-      b = c.html(),
-      a = 0,
-      d = 0;
-    c.html("");
-    var e = function() {
-      if ("<" == b.substring(a, a + 1)) {
-        var f = new RegExp(/<span class="instant"/),
-          g = new RegExp(/<span class="clear"/);
-        if (b.substring(a, b.length).match(f)) a += b.substring(a, b.length).indexOf("</span>") + 10;
-        else if (b.substring(a, b.length).match(g)) d = a, a += b.substring(a, b.length).indexOf("</span>") + 10;
-        else
-          for (;
-            ">" != b.substring(a, a + 1);) a++
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
       }
-      c.html(b.substring(d, a++) + (a & 1 ? "_" : ""));
-      a >= b.length || setTimeout(e, 70 + 100 *
-        Math.random())
-    };
-    e()
-  });
-  return this
-};
-$(".terminal").typewriter();
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+})()
